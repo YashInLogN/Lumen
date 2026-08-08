b400414c92f09a9c4267561b90f15a4bd07ecdb2
YashBuilds <chaudharyyashkakran@gmail.com>
2026-08-08 17:11:16 +0530

    Created file for arrays

---
+++ run.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+# data = np.abs(np.random.randn(100, 3))   Generate random data
+# df = pd.DataFrame(data, columns=['A', 'B', 'C'])  Display the first few rows of the DataFrame
+# print(df)
+
+# print("\nSummary Statistics:")
+# print(df.describe())   Display summary statistics of the DataFrame
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(np.mean(arr))  # Calculate and print the mean of the array
+

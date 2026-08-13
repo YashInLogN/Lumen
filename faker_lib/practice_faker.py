6aa21b6cf2ec0e7e7f2f785ede8e6a7fd08cffe4
YashBuilds <chaudharyyashkakran@gmail.com>
2026-08-13 23:54:57 +0530

    Created basic dataframe using faker().

--- faker_lib/practice_faker.py
+++ faker_lib/practice_faker.py
@@ -1,9 +1,19 @@
 # let get into faker mode😁
+import pandas as pd
+import numpy as np
+from faker import Faker
 
 print("Practice started...!")
 
-from faker import Faker
-
 fake = Faker()
 
-print(fake.email())
+names = [(lambda: fake.name())()for _ in range(10)]
+email = [(lambda: fake.email())() for _ in range(10)]
+mob = [(lambda: fake.phone_number())() for _ in range(10)]
+
+df = pd.DataFrame({'Name' : names,'EMAIL' : email,'MOB' : mob})
+
+print(df.to_string)
+
+
+
